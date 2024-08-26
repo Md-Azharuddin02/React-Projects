123cf3431e12139f316e5a51814682f709af454c
Md Azharuddin <mdazharuddin155332@gmail.com>
2024-08-26 19:28:44 +0530

    fix the bug

--- Accordion/src/App.jsx
+++ Accordion/src/App.jsx
@@ -1,11 +1,16 @@
-import { Container } from 'postcss'
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
+  const [count, setCount] = useState(0)
 
   return (
     <>
-     <Container/>
+      <h1 className="text-3xl text-red-700 font-bold underline">
+      Hello world!
+    </h1>
     </>
   )
 }
